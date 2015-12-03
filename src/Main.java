727cd2833d9467cf6e4c88dbba4e02085717392d
MarcosGrzeca <marcos-a-g@hotmail.com>
2015-12-03 16:06:24 -0200

    Tela de simulação

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,22 @@
+import javax.swing.JFrame;
 
 public class Main {
 
-	public static void main(String[] args) {
-
+	public static TelaInicial frameTelaInicial;
+	
+	public static void iniciarSimulacao() {
 		Ambiente amb = Ambiente.getInstance();
 		amb.init();
 		amb.setVisible(true);
-
+		frameTelaInicial.dispose();
+    }
+    
+	public static void main(String[] args) {
+        //Iniciar tela para informar parametros
+        frameTelaInicial = new TelaInicial();
+        frameTelaInicial.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);      
+        frameTelaInicial.setLocationByPlatform(true);
+        frameTelaInicial.setVisible(true);
+        frameTelaInicial.setTitle("Simulação Resgate");
 	}
 }
